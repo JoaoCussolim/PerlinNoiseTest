746f1dbebdcd8efc5ba8995d22d02544b50a149a
JoaoCussolim <joaovccussolim@gmail.com>
2024-08-28 13:02:41 -0300

    Functions update
    Function updated

--- index.js
+++ index.js
@@ -46,23 +46,25 @@ addEventListener('keydown', (e) => {
         offsetX += 5;
     }
     if (e.key === 'q') {
-        if (scale > 0.5) scale -= 0.1;
+        if (octaveCount < 9) octaveCount++;
     }
     if (e.key === 'e') {
-        scale += 0.1;
+        if (octaveCount > 1) octaveCount--;
+    }
+    if (e.key === '1') {
+        mode = 1;
+    }
+    if (e.key === '2') {
+        mode = 2;
     }
 })
 
 let lineSpace = 50;
 
-let width = screenToWorldX(canvas.width) + lineSpace;
-let height = screenToWorldY(canvas.height) + lineSpace;
 
 let canvasResize = (window) => {
     canvas.width = window.innerWidth
     canvas.height = window.innerHeight
-    width = screenToWorldX(canvas.width) + lineSpace;
-    height = screenToWorldY(canvas.height) + lineSpace;
 }
 
 let drawSquares = (x, y) => {
@@ -70,10 +72,10 @@ let drawSquares = (x, y) => {
     let rng = new RandomNumberGenerator(seed)
     let position = { x: x, y: y }
     let size = lineSpace * scale
-    let center = {
-        x: position.x - size / 2,
-        y: position.y - size / 2
-    }
+    // let center = {
+    //     x: position.x - size / 2,
+    //     y: position.y - size / 2
+    // }
     let r = rng.nextInt(0, 255);
     let g = rng.nextInt(0, 255);
     let b = rng.nextInt(0, 255);
@@ -81,7 +83,7 @@ let drawSquares = (x, y) => {
     ctx.fillRect(worldToScreenX(position.x), worldToScreenY(position.y), size, size)
 }
 
-let drawGrid  = () => {
+let drawGrid = () => {
     const stw0x = screenToWorldX(0);
     const stw0y = screenToWorldY(0);
 
@@ -123,56 +125,28 @@ let drawPixels = () => {
     }
 }
 
+let noiseGenerated = false
 const rng = new RandomNumberGenerator()
 
-let nOutputSize = screenToWorldX(canvas.width);
-let noiseSeed1D = new Array(nOutputSize)
-let perlinNoise1D = new Array(nOutputSize)
-for(let i = 0; i < nOutputSize; i++){
-    noiseSeed1D[i] = rng.nextFloat(0, 1)
-}
+let noiseSeed2D = new Array((canvas.width) * (canvas.height))
+let perlinNoise2D = new Array((canvas.width) * (canvas.height))
+let nOutputWidth = (canvas.width)
+let nOutputHeight = (canvas.height)
 
-let noiseSeed2D = new Array(canvas.width * canvas.height)
-let perlinNoise2D = new Array(canvas.width * canvas.height)
-let nOutputWidth = screenToWorldX(canvas.width)
-let nOutputHeight = screenToWorldY(canvas.height)
-let mode = 2
-
-for(let i = 0; i < canvas.width * canvas.height; i++){
+for (let i = 0; i < (canvas.width) * (canvas.height); i++) {
     noiseSeed2D[i] = rng.nextFloat(0, 1)
 }
 
-let octaveCount = 5
+let octaveCount = 2
 
-let drawPerlinNoise1D = (nCount, seed, nOctaves, fOutput) => {
-    for(let x = 0; x < nCount; x++){
-        let fNoise = 0;
-        let scaleAcc = 0
-        let perlinScale = 1
-        let nPitch = nCount;
-        for(let o = 0; o < nOctaves; o++){
-            nPitch = Math.max(1, Math.floor(nPitch / 2));
-            let nSample1 = Math.floor(x / nPitch) * nPitch;
-            let nSample2 = (nSample1 + nPitch) % nCount
-            let fBlend = (x - nSample1) / nPitch
-            let fSample = (1 - fBlend) * seed[nSample1] + fBlend * seed[nSample2]
-            fNoise += fSample * perlinScale
-            scaleAcc += perlinScale
-
-            perlinScale /= 2
-        }
-        fOutput[x] = fNoise / scaleAcc
-    }
-}
-
-let drawPerlinNoise2D = (nWidth, nHeight, seed, nOctaves, fOutput) => {
-    for(let x = 0; x < nWidth; x++){
-        for(let y = 0; y < nHeight; y++){
+let createPerlinNoise2D = (initialX, initialY, nWidth, nHeight, seed, nOctaves, fOutput) => {
+    for (let x = initialX; (x) < nWidth; x += lineSpace) {
+        for (let y = initialY; (y) < nHeight; y += lineSpace) {
             let fNoise = 0;
             let scaleAcc = 0
             let perlinScale = 1
             let nPitch = nWidth;
-            for(let o = 0; o < nOctaves; o++){
+            for (let o = 0; o < nOctaves; o++) {
                 nPitch = Math.max(1, Math.floor(nPitch / 2));
                 let nSample1X = Math.floor(x / nPitch) * nPitch;
                 let nSample1Y = Math.floor(y / nPitch) * nPitch;
@@ -181,69 +155,97 @@ let drawPerlinNoise2D = (nWidth, nHeight, seed, nOctaves, fOutput) => {
                 let fBlendX = (x - nSample1X) / nPitch
                 let fBlendY = (y - nSample1Y) / nPitch
 
-                let fSampleT = (1 - fBlendX) * seed[nSample1Y * nWidth + nSample1X] + fBlendX * seed[nSample1Y * nWidth + nSample2X ]
-                let fSampleB = (1 - fBlendX) * seed[nSample2Y * nWidth + nSample1X] + fBlendX * seed[nSample2Y * nWidth + nSample2X ]
+                let fSampleT = (1 - fBlendX) * seed[nSample1Y * nWidth + nSample1X] + fBlendX * seed[nSample1Y * nWidth + nSample2X]
+                let fSampleB = (1 - fBlendX) * seed[nSample2Y * nWidth + nSample1X] + fBlendX * seed[nSample2Y * nWidth + nSample2X]
                 fNoise += (fBlendY * (fSampleB - fSampleT) + fSampleT) * perlinScale
                 scaleAcc += perlinScale
 
                 perlinScale /= 2
             }
             fOutput[y * nWidth + x] = fNoise / scaleAcc
-            }
         }
+    }
+}
+
+let drawNoise = () => {
+
+    const stw0x = screenToWorldX(0);
+    const stw0y = screenToWorldY(0);
+
+    const initialX = Math.floor(stw0x / lineSpace) * lineSpace;
+    const initialY = Math.floor(stw0y / lineSpace) * lineSpace;
+
+    nOutputWidth = screenToWorldX(canvas.width)
+    nOutputHeight = screenToWorldY(canvas.height)
+
+    const finalX = Math.ceil(nOutputWidth / lineSpace) * lineSpace;
+    const finalY = Math.ceil(nOutputHeight / lineSpace) * lineSpace;
+
+    if(!noiseGenerated){
+        createPerlinNoise2D(initialX, initialY, finalX, finalY, noiseSeed2D, octaveCount, perlinNoise2D);
+        noiseGenerated = true
+    }
+
+
+    for (let x = initialX; x < finalX; x += lineSpace) {
+        for (let y = initialY; y < finalY; y += lineSpace) {
+            let pixelBw = Math.floor(perlinNoise2D[y * nOutputWidth + x] * 12)
+            let bg_col = 'grey'
+            switch (pixelBw) {
+                case 0:
+                    bg_col = 'black'
+                    break;
+                case 1:
+                    bg_col = 'white'
+                    break;
+                case 2:
+                    bg_col = 'blue'
+                    break;
+                case 3:
+                    bg_col = 'red'
+                    break;
+                case 4:
+                    bg_col = 'cyan'
+                    break;
+                case 5:
+                    bg_col = 'yellow'
+                    break;
+                case 6:
+                    bg_col = 'green'
+                    break;
+                case 7:
+                    bg_col = 'purple'
+                    break;
+                case 8:
+                    bg_col = 'brown'
+                    break;
+                case 9:
+                    bg_col = 'pink'
+                    break;
+                case 10:
+                    bg_col = 'orange'
+                    break;
+            }
+            ctx.fillStyle = bg_col
+            ctx.fillRect(worldToScreenX(x), worldToScreenY(y), lineSpace, lineSpace)
+            console.log(`Normal X and Y: ${x}, ${y}. WorldPoints X and Y: ${worldToScreenX(x)}, ${worldToScreenY(y)}`)
+        }
+    }
 }
 
 let animate = () => {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     //drawPixels();
-    //drawGrid();
-
-    if(mode === 1){    
-        drawPerlinNoise1D(nOutputSize, noiseSeed1D, octaveCount, perlinNoise1D);
-
-
-        for(let x = 0; x < nOutputSize; x++){
-            let y = -(perlinNoise1D[x] * screenToWorldY(canvas.height)/2 + (screenToWorldY(canvas.height)/2))
-            for(let f = y; f < screenToWorldY(canvas.height) / 2; f += lineSpace){
-                drawLine(x, y, x, 50, 'white')
-            }
-        }
-    }
-
-    if(mode === 2){    
-        drawPerlinNoise2D(nOutputWidth, nOutputHeight, noiseSeed2D, octaveCount, perlinNoise2D);
-
-
-        for(let x = 0; x < nOutputWidth; x++){
-            for(let y = 0; y < nOutputHeight; y++){
-                let pixelBw = perlinNoise2D[y * nOutputWidth + x] * 4
-                let bg_col = 'white'
-                switch(pixelBw){
-                    case 0: 
-                        bg_col = 'black'
-                        break;
-                    case 1:
-                        bg_col = 'white'
-                        break;
-                    case 2:
-                        bg_col = 'blue'
-                        break;
-                    case 3:
-                        bg_col = 'red'
-                        break;
-                    case 4:
-                        bg_col = 'cyan'
-                        break;
-                }
-                ctx.fillStyle= bg_col;
-                ctx.fillrect(x, y, 50, 50);
-            }
-        }
-    }
-
+    drawNoise();
+    drawGrid();
+    
     requestAnimationFrame(animate);
 }
 
 addEventListener("resize", (event) => { canvasResize(window) });
 
 animate();
+
+// for (let i = noiseSeed2D.length - 1; i < nOutputWidth * nOutputHeight; i--) {
+//     if(!noiseSeed2D[i]) noiseSeed2D[i] = rng.nextFloat(0, 1)
+// }
